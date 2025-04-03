7952a55864c5e6877daeb2f448061b1d862f5b79
Pablo <PabloHAlves99@gmail.com>
2025-04-02 21:03:58 -0300

    Update app.py

--- apiRest/flask/app.py
+++ apiRest/flask/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
+api = Api(app)
+jwt = JWTManager(app)
 
 
 @app.route('/')
